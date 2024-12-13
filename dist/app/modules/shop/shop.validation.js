24f3031d13d2eba16289724cc61ef1a32f1cf80e
wahid1099 <wahidahmed890@gmail.com>
2024-12-13 16:26:51 +0600

    shop craate

--- dist/app/modules/shop/shop.validation.js
+++ dist/app/modules/shop/shop.validation.js
@@ -7,7 +7,6 @@ exports.ShopValidationSchema = {
         name: zod_1.z.string().min(1, { message: "Shop name is required" }),
         logo: zod_1.z.string().url().optional(),
         description: zod_1.z.string().optional(),
-        vendorId: zod_1.z.string().min(1, { message: "Vendor ID is required" }), // Add vendorId here
     }),
     updateShopSchema: zod_1.z.object({
         name: zod_1.z.string().optional(),
--- src/app/modules/shop/shop.validation.ts
+++ src/app/modules/shop/shop.validation.ts
@@ -5,7 +5,6 @@ export const ShopValidationSchema = {
     name: z.string().min(1, { message: "Shop name is required" }),
     logo: z.string().url().optional(),
     description: z.string().optional(),
-    vendorId: z.string().min(1, { message: "Vendor ID is required" }), // Add vendorId here
   }),
   updateShopSchema: z.object({
     name: z.string().optional(),
