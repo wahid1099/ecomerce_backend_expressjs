98fac30d0b8d71d674e0068c7e9f548f15043cdc
wahid1099 <wahidahmed890@gmail.com>
2024-12-18 09:12:11 +0600

    abfsdfsad

--- dist/app/modules/Payment/payment.route.js
+++ dist/app/modules/Payment/payment.route.js
@@ -5,10 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PaymentRoute = void 0;
 const express_1 = __importDefault(require("express"));
-const payment_controller_1 = require("./payment.controller");
 const router = express_1.default.Router();
-router.post("/create-payment", payment_controller_1.PaymentControllers.createPayment);
-router.post("/confirmation", payment_controller_1.PaymentControllers.confirmationController);
-router.get("/get-all", payment_controller_1.PaymentControllers.getAllPayment);
-router.get("/get-single/:id", payment_controller_1.PaymentControllers.getSinglePayment);
+// router.post("/create-payment", PaymentControllers.createPayment);
+// router.post("/confirmation", PaymentControllers.confirmationController);
+// router.get("/get-all", PaymentControllers.getAllPayment);
+// router.get("/get-single/:id", PaymentControllers.getSinglePayment);
 exports.PaymentRoute = router;
--- src/app/modules/Payment/payment.route.ts
+++ src/app/modules/Payment/payment.route.ts
@@ -3,12 +3,12 @@ import { PaymentControllers } from "./payment.controller";
 
 const router = express.Router();
 
-router.post("/create-payment", PaymentControllers.createPayment);
+// router.post("/create-payment", PaymentControllers.createPayment);
 
-router.post("/confirmation", PaymentControllers.confirmationController);
+// router.post("/confirmation", PaymentControllers.confirmationController);
 
-router.get("/get-all", PaymentControllers.getAllPayment);
+// router.get("/get-all", PaymentControllers.getAllPayment);
 
-router.get("/get-single/:id", PaymentControllers.getSinglePayment);
+// router.get("/get-single/:id", PaymentControllers.getSinglePayment);
 
 export const PaymentRoute = router;
